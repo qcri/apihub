43b8966c0c8c001e2bd842df4396d83f368d3970
AhmedMahdy14 <ahmedmahdy1420@gmail.com>
2022-11-13 09:21:49 +0000

    -Result testing breaks

--- apihub/result.py
+++ apihub/result.py
@@ -3,9 +3,13 @@ from prometheus_client import Counter, Histogram
 from dotenv import load_dotenv
 
 from pipeline import ProcessorSettings, Processor, Command, CommandActions, Definition
+
+from apihub.activity.queries import ActivityQuery
+from apihub.activity.schemas import ActivityStatus
+from apihub.common.db_session import db_context
 from apihub.utils import Result, Status, RedisSettings, DefinitionManager
 from apihub import __worker__, __version__
-
+import pdb
 
 load_dotenv()
 
@@ -52,6 +56,9 @@ class ResultWriter(Processor):
 
     def process(self, message_content, message_id):
         self.logger.info("Processing MESSAGE")
+        # pdb.set_trace()
+        if "content" in message_content:
+            message_content = message_content.get("content")
         result = Result.parse_obj(message_content)
         if result.status == Status.PROCESSED:
             result.result = {
@@ -63,7 +70,16 @@ class ResultWriter(Processor):
         if self.redis.get(message_id) is not None:
             self.logger.warning("Found result with key %s, overwriting...", message_id)
 
-        self.redis.set(message_id, result.json(), ex=86400)
+        r = result.json()
+        self.redis.set(message_id, r, ex=86400)
+        with db_context() as session:
+            pdb.set_trace()
+            ActivityQuery(session).update_activity(message_id, **{"status": ActivityStatus.PROCESSED})
+            # activity = ActivityQuery(session).get_activity_by_key(message_id)
+            # activity.result = str(r)
+            # activity.status = ActivityStatus.PROCESSED
+            # session.commit()
+
         return None
 
 
