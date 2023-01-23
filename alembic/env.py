9fafbb57e65b0a5ca6592e87eafd0745db6107eb
Yifan Zhang <freqyifan@gmail.com>
2023-01-23 14:26:42 +0300

    alembic: read database url from env

--- alembic/env.py
+++ alembic/env.py
@@ -15,7 +15,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option('sqlalchemy.url', os.environ.get('DB_URI'))
+config.set_main_option('sqlalchemy.url', os.environ.get('DB_URI').replace('%', '%%'))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
