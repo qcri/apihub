20945d5b0a116b0f1a35f800f5d52e27efcacd00
Yifan Zhang <freqyifan@gmail.com>
2023-01-25 11:32:14 +0300

    cli: Add load_data to admin

--- apihub/admin.py
+++ apihub/admin.py
@@ -2,13 +2,14 @@ import sys
 
 from pydantic import BaseSettings
 
-from apihub.common.db_session import db_context, Base, DB_ENGINE
+from apihub.common.db_session import db_context, Base, DB_ENGINE, create_session
 from apihub.common.redis_session import redis_conn
-from apihub.security.schemas import UserCreate, UserType
+from apihub.security.schemas import UserCreate, UserType, ProfileBase
 from apihub.security.queries import UserQuery
 from apihub.security.models import User, Profile
 from apihub.subscription.queries import SubscriptionQuery
 from apihub.subscription.models import Application, Subscription, Pricing
+from apihub.subscription.schemas import ( ApplicationCreate, SubscriptionCreate, )
 
 
 class SuperUser(BaseSettings):
@@ -54,22 +55,55 @@ def deinit():
 
 def load_data(filename):
     import yaml
-    data = yaml.load(open(filename, 'r', encoding='utf-8'),)
-    for name, items in data.items():
-        if name == 'user':
-            with db_context() as session:
-                query = UserQuery(session)
+    data = yaml.safe_load(open(filename, 'r', encoding='utf-8'),)
+    with db_context() as session:
+        users = {}
+        applications = {}
+        pricings = {}
+        subscriptions = {}
+        for name, items in data.items():
+            if name == 'user':
                 for item in items:
-                    user_id = query.create_user(
-                        UserCreate(
-                            name=item.name,
-                            email=item.email,
-                            password=item.password,
-                            role=item.role,
-                        )
+                    profile_data = ProfileBase(**item)
+                    name = profile_data.first_name + " " + profile_data.last_name
+                    user_data = UserCreate(name=name, **item)
+                    user = User(**user_data.make_user().dict())
+                    session.add(user)
+
+                    profile = Profile(
+                        user_id=user.id,
+                        **profile_data.dict(),
                     )
-                    if user_id:
-        elif name == 'application':
-            for item in items:
-        else:
-            print(f"model {name} not supported", file=sys.stderr)
+                    session.add(profile)
+                    users[user.email] = user
+            elif name == 'application':
+                for item in items:
+                    application_data = ApplicationCreate(**item)
+                    pricings = []
+                    for pricing in item['pricings']:
+                        pricing = Pricing(
+                            tier=pricing['tier'],
+                            credit=pricing['credit'],
+                            price=pricing['price'],
+                        )
+                        pricings.append(pricing)
+                    application = Application(
+                        name=application_data.name,
+                        url=application_data.url,
+                        description=application_data.description,
+                        user_id=users[item['user']].id,
+                        pricings=pricings,
+                    )
+                    session.add(application)
+                    applications[application.name] = application
+            # elif name == 'subscription':
+            #     for item in items:
+            #         subscription = Subscription(
+            #             user_id=users[item.user].id,
+            #             application_id=applications[item.application].id,
+            #             pricing_id=pricings[item.pricing].id,
+            #             expires_at=item.expires_at,
+            #         )
+            #         subscriptions[subscription.user_id] = subscription
+            else:
+                print(f"model {name} not supported", file=sys.stderr)
